72928fa2f5e6ae67e462fa6a13a1b2d4c150fd25
aditnag <iamaditnag034@gmail.com>
2022-08-10 09:32:57 +0530

    db password closed

--- office_emp_project/settings.py
+++ office_emp_project/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'OfficeEmpManagementSystem',
         'USER': 'postgres',
-        'PASSWORD': 'Luffy123',
+        'PASSWORD': '########',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
